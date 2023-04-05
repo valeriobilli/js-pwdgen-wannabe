fd0689eea331422147b5af276a99005856ca5e99
Valerio Billi <valerio.billi18@gmail.com>
2023-04-05 17:39:45 +0200

    bozza javascript

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let firstName = prompt("Inserisci il tuo nome")
+let lastName = prompt ("Inserisci il tuo cognome")
+let color = prompt ("Qual è il tuo colore preferito?")
+let year = prompt ("In che anno sei nato?")
+
+document.getElementById("psw").innerText = firstName + lastName + color + year
