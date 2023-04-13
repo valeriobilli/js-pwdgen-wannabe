2f332931c2492e01d105abe2e74add70b86029bf
Valerio Billi <valerio.billi18@gmail.com>
2023-04-13 09:59:27 +0200

    correzione lezione

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 let firstName = prompt("Inserisci il tuo nome")
 let lastName = prompt ("Inserisci il tuo cognome")
 let color = prompt ("Qual è il tuo colore preferito?")
-let year = prompt ("In che anno sei nato?")
 
-document.getElementById("psw").innerText = firstName + lastName + color + year
+let password = firstName + "-" + lastName + "-" + color + "2023"
+
+console.log("La nuova password è: ", password)
+
+document.getElementById("psw").innerText = password;
