4c137b68449c7ab309b756b5af7a28ba4b537247
Valerio Billi <valerio.billi18@gmail.com>
2023-04-18 15:48:00 +0200

    first commit

--- js/main.js
+++ js/main.js
@@ -2,8 +2,46 @@ let firstName = prompt("Inserisci il tuo nome")
 let lastName = prompt ("Inserisci il tuo cognome")
 let color = prompt ("Qual è il tuo colore preferito?")
 
-let password = firstName + "-" + lastName + "-" + color + "2023"
+let firstNameOk = false;
+let lastNameOk = false;
+let colorOk = false;
 
-console.log("La nuova password è: ", password)
+if (firstName == null || firstName == "") {
+    alert("Non hai inserito il nome");
+} else if (firstName.length < 2) {
+    alert("Il nome è troppo corto");
+} else {
+    console.log("L'utente ha inserito un nome valido");
+    firstNameOk = true;
+}
+
+if (lastName == null || lastName == "") {
+    alert("Non hai inserito il cognome");
+} else if (lastName.length < 2) {
+    alert("Il nome è troppo corto");
+} else {
+    console.log("L'utente ha inserito un cognome valido");
+    lastNameOk = true;
+}
+
+if (color == null || color == "") {
+    alert("Non hai inserito il colore");
+} else {
+    console.log("L'utente ha inserito un colore valido");
+    colorOk = true;
+}
+
+let password = firstName + "-" + lastName + "-" + color + "-2023";
+
+if (firstNameOk && lastNameOk && colorOk ) {
+    console.log("La nuova password è: ", password);
+} else {
+    console.error ("Impossibile creare una password, dati mancanti")
+}
 
 document.getElementById("psw").innerText = password;
+
+
+
+
+
